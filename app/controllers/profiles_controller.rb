753d612fbb77812cd0de07e34dce0df5671a95d6
Lincoln Marcolongo <mlincoln1205@gmail.com>
2021-05-25 09:34:30 +0200

    first commit

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,68 @@
+class ProfilesController < ApplicationController
+  before_action :find_profile, only: [:show, :private_page, :edit, :update, :change_privacy]
+  before_action :authenticate_user!
+  before_action :public?, except: %i[private_page, show]
+ 
+  def show
+  end
+
+  def new
+    @profile = Profile.new
+  end
+
+  def create 
+    @profile = Profile.create(profile_params)
+    @profile.user = current_user
+    current_user.profile_id = @profile.id
+    if @profile.save
+      flash[:notice] = 'Profile Created!'
+      redirect_to @profile
+    else
+      render :new
+    end
+  end
+
+  def edit 
+  end
+
+  def update
+    if @profile.update(profile_params)
+      flash[:notice] = 'Task Updated!'
+      redirect_to @profile
+    else
+      render :edit
+    end 
+  end
+
+  def change_privacy
+    @profile.update(privacy_params)
+    redirect_to @profile
+  end
+
+  def private_page
+  end
+
+  private 
+
+  def profile_params
+    params.require(:profile).permit(:nickname, :bio, :avatar)
+  end 
+
+  def privacy_params
+    params.require(:profile).permit(:share)
+  end
+
+  def find_profile
+    @profile = Profile.find(params[:id])
+  end
+
+  def public?
+    unless (current_user.profile == @profile)
+      unless @profile.share
+        redirect_to private_page_profile_path(@profile)
+      end
+    end
+  end
+
+end
+  
